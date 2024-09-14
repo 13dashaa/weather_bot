102a44e5c49a952c17a11d21ac83f9de1488d096
13dashaa <168581575+13dashaa@users.noreply.github.com>
2024-09-14 20:41:03 +0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
 import telebot
-BOT_TOKEN = telebot.TeleBot('7496257139:AAGoma9O_A2NZjC0oNYUQzSntWXq2opyfzY')
-API = 'f42a25ea3769b7bce03b4493a2add6c3'
+BOT_TOKEN = telebot.TeleBot('99999999999:AAGoma9O_A2NZjC0oNYUQzSntWXq2opyfzY')
+API = 'f42a25ea3769b7bce03b4493999999999999'
